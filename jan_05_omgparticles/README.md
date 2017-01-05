20c5476e8b8c2059eb78d6a27817c759a5e01dec
Bomoko <blaize.kaye@gmail.com>
2017-01-05 14:29:13 +0200

    Added 5th

---
+++ jan_05_omgparticles/README.md
@@ -0,0 +1,5 @@
+# 5th Jan 2017
+
+Saw Mike Bostock's [OMG Particles!](http://bl.ocks.org/mbostock/1062544) and thought I'd take a swing at doing something similar. His implementation is pretty dope, so I'd recommend you read _that_ rather than mine if you're interested in source.
+
+![Screenshot](screenshot.png) 
---
+++ jan_05_omgparticles/sketch.js
@@ -0,0 +1,76 @@
+var particleArray = [];
+var colorGens = { r: new colorGenerator(1), g: new colorGenerator(2), b: new colorGenerator(3)};
+
+function setup() {
+  createCanvas(800, 800);
+  background(0);  
+}
+
+function draw() {
+  background(0);
+  particleArray.forEach(function(p, i) {
+    if(p.isAlive()) {
+      p.draw();
+    } else {
+      particleArray.splice(i,1);
+    }
+  });
+}
+
+function colorGenerator(difference) {
+  var currentVal = 0;
+  var goingUp = true;
+  this.nextVal = function() {
+    if(goingUp) {
+      currentVal += difference;
+      if(currentVal >= 255) {
+        currentVal = 255;
+        goingUp = false;
+      }
+    } else if (!goingUp && currentVal > 0) {
+      currentVal -= difference;
+      if(currentVal <= 0) {
+        currentVal = 0;
+        goingUp = true;
+      }
+    }
+    return currentVal;
+  };
+}
+
+function mouseMoved() {
+  if(mouseX > 0 &&
+    mouseX < width &&
+    mouseY > 0&&
+    mouseY < height) {
+      particleArray.push(new particle(mouseX, mouseY,
+      colorGens.r.nextVal(), colorGens.g.nextVal(), colorGens.b.nextVal()));
+    }
+}
+
+
+function particle(x, y, r, g, b) {
+  var r = 0;
+  var amIAlive = true;
+  
+  this.draw = function() {
+    if(amIAlive) {
+    noFill();
+    strokeWeight(2);
+    stroke(r, g, b, 255 - r);
+    ellipse(x, y, r);
+    r += 2;
+    if(r >= 255) {
+        amIAlive = false;
+      }
+    }
+  };
+  
+  this.isAlive = function() {
+    return amIAlive;
+  };
+}
+
+function mousePressed() {
+  console.log("Number of particles in array: " + particleArray.length);
+}
